c0eef94bdd6d4b8d24add7f25b89cdc0ba2ff82a
Travis Miller <travis@spark.us>
2014-03-02 18:04:26 -0500

    Feed Fucntionality Updates and Search Page Created

--- feed.php
+++ feed.php
@@ -1,5 +1,5 @@
 <?php include "include/header.php" ?>
-  <div data-role="page">
+  <div id="feedview" data-role="page">
 
   <?php include "include/menu-panel.php" ?>
 
@@ -9,8 +9,8 @@
 
         <div data-role="navbar">
           <ul>
-            <li><a href="#">Feed</a></li>
-            <li><a href="#">Grid</a></li>
+            <li><a href="#feedview">Feed</a></li>
+            <li><a href="#gridview">Grid</a></li>
           </ul>
         </div><!-- /navbar -->
       
@@ -36,7 +36,22 @@
 
   </div>
 
-<div data-role="page">
+<div id="gridview" data-role="page">
+
+    <div data-role="header" data-position="fixed">
+    <a data-iconpos="notext" href="#panel" data-role="button" data-icon="flat-menu"></a>
+      <h1>Feed</h1>
+
+        <div data-role="navbar">
+          <ul>
+            <li><a href="#feedview">Feed</a></li>
+            <li><a href="#gridview">Grid</a></li>
+          </ul>
+        </div><!-- /navbar -->
+      
+    </div>
+
+
     <div class="ui-grid-b">
       <div class="ui-block-a"><a href="../edit-select.php" data-transition="slide"><div class="ui-bar ui-bar-a" style="height:80px; margin:10px;">Seating & Tables</div></a></div>
        <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:80px; margin:10px;">Food</div></div>
@@ -44,6 +59,9 @@
        <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:80px; margin:10px;">Food</div></div>
       
     </div>
+
+    <?php include "include/menu-footer.php" ?>
+    
 </div>
 
 <?php include "include/footer.php" ?>
